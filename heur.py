7219883fc8a4425c15e65bc991ee751c90850f14
unknown <ruben.heeroo@gmail.com>
2018-10-11 17:50:53 +0200

    :

--- heur.py
+++ heur.py
@@ -10,7 +10,7 @@ p = time.strftime(%Y, time.localtime(now))
 print(p)"""
 a= datetime.date(2018, 1, 1)
 for i in range(0,365):
-    if a.weekday()==4
+    if a.weekday()==4:
         print (a)
 #b = datetime.weekday(annee, 1, 1)
 print(a)
