bfcec45c7eb040445e1cc01ca83a57e26b337b26
unknown <ruben.heeroo@gmail.com>
2018-10-11 17:29:25 +0200

    first commit

---
+++ heur.py
@@ -0,0 +1,20 @@
+import time, datetime
+
+"""print(time.time())
+
+help(time)
+
+print(time.localtime())
+print(datetime.datetime.now())
+p = time.strftime(%Y, time.localtime(now))
+print(p)"""
+a= datetime.date(2018, 1, 1)
+for i in range(0,365):
+    if a.weekday()==4
+        print (a)
+#b = datetime.weekday(annee, 1, 1)
+print(a)
+#print (b)
+
+print(a.weekday())
+
